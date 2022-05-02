9b58c761d2cc19e4ed8a555db1a6ddcb65452005
SIO2 <yanis.halaoui@yanish.fr>
2022-05-02 16:32:02 +0200

    first commit

---
+++ AP_Gestion_HALAOUI/View/UI_Login.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AP_Gestion_HALAOUI.View
+{
+    /// <summary>
+    /// Logique d'interaction pour UI_Login.xaml
+    /// </summary>
+    public partial class UI_Login : UserControl
+    {
+        public UI_Login()
+        {
+            InitializeComponent();
+        }
+    }
+}
