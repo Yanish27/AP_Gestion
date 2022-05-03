c3558fb1a93ee8c5691b1ef164d6c2389ab95fe2
Yanis HALAOUI <yanis.halaoui@saintmichelannecy.fr>
2022-05-03 22:35:36 +0200

    DAO

--- AP_Gestion_HALAOUI/View/UI_Login.xaml.cs
+++ AP_Gestion_HALAOUI/View/UI_Login.xaml.cs
@@ -10,7 +10,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using AP_Gestion_HALAOUI.BDD;
+using AP_Gestion_HALAOUI.View;
+using AP_Gestion_HALAOUI.DAO;
+using AP_Gestion_HALAOUI.Controller;
 namespace AP_Gestion_HALAOUI.View
 {
     /// <summary>
@@ -18,9 +21,43 @@ namespace AP_Gestion_HALAOUI.View
     /// </summary>
     public partial class UI_Login : UserControl
     {
+
         public UI_Login()
         {
             InitializeComponent();
+        }
+
+        private void BTN_Connexion_Click(object sender, RoutedEventArgs e)
+        {
+            if (TB_Username.Text != "")
+            {
+                if (TB_Password.Password != "")
+                {
+                    Utilisateur utilisateur = new Utilisateur();
+                    DAOEscapegame DAO = new DAOEscapegame();
+                    if (DAO.CheckUsername(TB_Username.Text) == true)
+                    {
+                        if (DAO.CheckUsernamePassword(TB_Username.Text, TB_Password.Password) == true)
+                        {
+                            MessageBox.Show("OK");
+                        } else
+                        {
+                            MessageBox.Show("Mot de passe incorrect");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Cet utilisateur existe pas pas pas");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Veuillez entrer un mot de passe");
+                }
+            } else
+            {
+                MessageBox.Show("Veuillez entrer un nom d'utilisateur");
+            }
         }
     }
 }
